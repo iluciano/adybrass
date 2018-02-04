99bf8caee8f170b1be5ff9ada14618ac616855ec
Igor Luciano <igorluciano@igorluciano.com.br>
2018-02-04 15:34:20 -0200

    Contato e localização

--- js/adyjs.js
+++ js/adyjs.js
@@ -1,15 +1,21 @@
 $("#collapse1").click(function(){
-    $( "#multiCollapse1" ).toggle();
-    $("#multiCollapse2").collapse("hide");
-    $("#multiCollapse3").collapse("hide");
+    $("#multiCollapse1").toggle();
+    $("#multiCollapse2").hide();
+    $("#multiCollapse3").hide();
 });
 $("#collapse2").click(function(){
-    $( "#multiCollapse2" ).toggle();
-    $("#multiCollapse1").collapse("hide");
-    $("#multiCollapse3").collapse("hide");
+    $("#multiCollapse2").toggle();
+    $("#multiCollapse1").hide();
+    $("#multiCollapse3").hide();
 });
 $("#collapse3").click(function(){
-    $( "#multiCollapse3" ).toggle();
-    $("#multiCollapse1").collapse("hide");
-    $("#multiCollapse2").collapse("hide");
+    $("#multiCollapse3").toggle();
+    $("#multiCollapse1").hide();
+    $("#multiCollapse2").hide();
+});
+$(".menu-nav-a").click(function(){
+    $("#navbarTogglerDemo01").toggle();
+});
+$(".navbar-toggler").click(function(){
+    $("#navbarTogglerDemo01").toggle();
 });
